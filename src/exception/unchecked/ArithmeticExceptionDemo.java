b4881dc41a53e8ba312311eb0f46899423e71fa0
danu <danujagreru@gmail.com>
2024-02-01 11:21:44 +0530

    try catch block

---
+++ src/exception/unchecked/ArithmeticExceptionDemo.java
@@ -0,0 +1,26 @@
+package exception.unchecked;
+
+/*
+    @author DanujaV
+    @created 2/1/24 - 11:10 AM   
+*/
+
+import java.util.Scanner;
+
+public class ArithmeticExceptionDemo {
+    public static void main(String[] args) {
+        int n1 = 10;
+        System.out.print("Enter a number: ");
+        int n2 = new Scanner(System.in).nextInt();
+
+
+        try {
+            int result = n1 / n2;   //ArithmeticException
+            System.out.println("Result: " + result);
+        } catch(ArithmeticException e) {
+            System.out.println("OOPS! Cannot divide by zero.");
+        }
+
+        System.out.println("End of program.");
+    }
+}
